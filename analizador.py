a289fa40edee259a4fbe5294c2f0acec9d0ba9a5
LucasGaleano <l.a.galeano2@gmail.com>
2018-01-13 16:09:13 -0300

    levanta el analizador todo a ram

--- analizador.py
+++ analizador.py
@@ -11,19 +11,10 @@
 #cerrar archivo
 
 import sys
+import time
 
-
-def esta_en_archivo(combinacion, archivoAnalizador):
-   #devuelve la posicion del archivo donde esta la combinacion y si no devuelve -1
-   archivoAnalizador.seek(0)
-   seekAnterior = 0
-   linea = archivoAnalizador.readline()
-   while linea != '':
-      if(linea.split(" ")[0]==combinacion):
-         return seekAnterior
-      seekAnterior = archivoAnalizador.tell()
-      linea = archivoAnalizador.readline()
-   return -1
+cantDeCeros=10
+g_diccionarioTransiciones={} #diccionario
 
 def sumar_transicion(seekUbicacion, archivoAnalizador):
    #devuelve la linea donde esta la transicion
@@ -32,49 +23,67 @@ def sumar_transicion(seekUbicacion, archivoAnalizador):
    archivoAnalizador.seek(seekUbicacion)
    archivoAnalizador.write(transicion.split()[0] + " " + str(int(transicion.split()[1])+1).zfill(20) + "\n")
 
-def agregar_combinacion(combinacion, archivoAnalizador):
+def agregar_combinacion(combinacionBuscar):
    #busca si esta la transicion en la lista y le suma 1,
    #si no esta lo agrega
-   
-   #se fija si la transicion esta en el archivo
-   seekUbicacion = esta_en_archivo(combinacion, archivoAnalizador)
-   if (seekUbicacion != -1 ):   
-      #esta la transicion, consige el valor y le suma 1
-      sumar_transicion(seekUbicacion, archivoAnalizador)
 
+
+   #se fija si la transicion esta en el archivo(diccionario)
+   if (g_diccionarioTransiciones.has_key(combinacionBuscar)):   
+      #si esta la transicion, consige el valor y le suma 1
+      g_diccionarioTransiciones[combinacionBuscar] += 1
    else:
-      #no esta la transicion, va al final del archivo y lo agrega
-      archivoAnalizador.seek(0, 2)
-      archivoAnalizador.write(combinacion + " " + str(1).zfill(20) + "\n")
+      #si no esta la transicion, lo agrega
+      g_diccionarioTransiciones[combinacionBuscar] = 1
 
-
-def dividir_en_transiciones(palabra, cantLetrasTransicion):
+def dividir_linea_en_transiciones(linea, cantLetrasTransicion):
    listaTransiciones=[]
-   indexPalabra = 0
-   longPalabra = len(palabra)
-   while longPalabra > indexPalabra:
+   indexlinea = 0
+   lengthLinea = len(linea)
+   while indexlinea<lengthLinea:     
       combinacion = ""
       for indexCombinacion in range(cantLetrasTransicion+1):
          try:
-            combinacion += palabra[indexCombinacion + indexPalabra]           
+            combinacion += linea[indexCombinacion + indexlinea]           
          except:
             break
          if(len(combinacion) != 1):
             listaTransiciones.append(combinacion)
-      indexPalabra += 1
+      indexlinea += 1
    return listaTransiciones
 
-def analizar_palabra(archivoAnalizador, palabra, cantLetrasTransicion):
-   #creo una lista de transiciones
-   listaTransiciones = dividir_en_transiciones(palabra,cantLetrasTransicion)
-   #agrego cada transicion al archivo
+def analizar_archivo_de_texto(archivoAnalizador,archivoTexto, cantLetrasTransicion):
+   g_diccionarioTransiciones = crear_diccionario_del_archivo_analizador(archivoAnalizador)
+   linea = archivoTexto.readline()
+   lineasAnalizadas = 0
+   while linea:
+      lineasAnalizadas += 1
+      analizar_linea(linea, cantLetrasTransicion)
+      print(str(lineasAnalizadas) + " lineas analizadas.")
+      linea = archivoTexto.readline()
+   escribirEnArchivo(archivoAnalizador);
+
+
+def analizar_linea(linea, cantLetrasTransicion):
+   listaTransiciones = dividir_linea_en_transiciones(linea,cantLetrasTransicion)
    for transicion in listaTransiciones:
       if(transicion.isalpha()):
-         agregar_combinacion(transicion,archivoAnalizador)
+         agregar_combinacion(transicion)
+         
 
 
+def crear_diccionario_del_archivo_analizador(archivoAnalizador):
+   linea = archivoAnalizador.readline() 
+   while linea != "":
+      transicion = linea.split(" ")
+      g_diccionarioTransiciones[transicion[0]] = int(transicion[1])
+      linea = archivoAnalizador.readline()
 
 
+def escribirEnArchivo(archivoAnalizador):
+   archivoAnalizador.seek(0)
+   for item in g_diccionarioTransiciones.items():
+      archivoAnalizador.write(str(item[0]) + " " +  str(item[1]) + "\n")
     
     
 
--- program.py
+++ program.py
@@ -3,19 +3,13 @@ import sys
 
 
 
-
+archivoTexto = open(sys.argv[1],'r')
 archivoAnalizador = open(sys.argv[2],'r+')
-with open(sys.argv[1],'r') as texto:
-   lineasAnalizadas = 0
-   linea = texto.readline()
-   while linea:
-      lineasAnalizadas += 1
-      palabras = linea.split(" ")
-      for palabra in palabras:
-         analizador.analizar_palabra(archivoAnalizador, palabra, int(sys.argv[3]))
+cantDeTransiciones = int(sys.argv[3])
 
-      print(str(lineasAnalizadas) + " lineas.")
-      linea = texto.readline()
+analizador.analizar_archivo_de_texto(archivoAnalizador, archivoTexto, cantDeTransiciones)
+
+
 
 archivoAnalizador.close()
 
